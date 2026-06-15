18f730c911a540e63a558c082a328aa7ad873d95
it26102118-lang <it26102118@my.sliit.lk>
2026-06-15 09:39:11 +0530

    Add files via upload

---
+++ Q3/IT26102118Lab2Q3.java
@@ -0,0 +1,8 @@
+public class labq3{
+	public static void main(String[]args){
+		double a=3.0;
+		double b=4.0;
+		double h=Math.sqrt((a*a)+(b*b));
+		System.out.println("Hypotenuse is"+":"+h);
+	}
+}
