37cc7c0241f0c5542321217b847585e5acdaa905
it26102118-lang <it26102118@my.sliit.lk>
2026-06-15 09:33:20 +0530

    Add files via upload

---
+++ Q1/IT26102118Lab2Q1.java
@@ -0,0 +1,9 @@
+public class labq1{
+	public static void main(String[]args){
+		int p=100;
+		double l=200.0/7.0;
+		double w=l*0.75;
+		System.out.println("length is"+":"+l);
+		System.out.println("width is"+":"+w);
+	}
+}
